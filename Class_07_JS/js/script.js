dd1e45491ecdbdb04a5bd3446cfe2b4fd176ee4a
IljaBg <ilja.buglovs@gmail.com>
2022-06-06 21:20:02 +0300

    comit js 1

---
+++ Class_07_JS/js/script.js
@@ -0,0 +1,9 @@
+/* function myFunction() {
+    document.getElementById("demo").innerHTML = "Paragraph changed.";
+    }
+    */
+
+456 || 0
+console.log(456 || 0);
+document.getElementById("demo").innerHTML = 456 || 0;
+
