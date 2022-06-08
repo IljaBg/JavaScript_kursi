62dd9b440d7200917d48170885eea6b78f32df69
IljaBg <ilja.buglovs@gmail.com>
2022-06-08 21:41:17 +0300

    js commit 08.06

--- Class_07_JS/js/script.js
+++ Class_07_JS/js/script.js
@@ -3,7 +3,7 @@
     }
     */
 
-456 || 0
-console.log(456 || 0);
-document.getElementById("demo").innerHTML = 456 || 0;
+    function myFunction() {
+        document.getElementById("demo").innerHTML = "Paragraph changed.";
+        }
 
