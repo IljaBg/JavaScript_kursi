3be381cf07a35509531ed96489ce1d90b821ddcf
IljaBg <ilja.buglovs@gmail.com>
2022-06-27 21:16:50 +0300

    commit switch2

---
+++ Class_12_switch/js/script.js
@@ -0,0 +1,28 @@
+x = 10;
+
+switch(x) {
+    case 9:
+        console.log("x ir precīzi 9");
+    case 10:
+        console.log("vai nu x ir precīzi 10 vai x ir bijis vienāds ar 9");
+    case 11:
+        console.log("vai nu x ir precīzi 11 vai ix ir bijis vienāds ar 10");
+    default:
+        console.log("default gadījums");
+}
+
+
+let y = prompt("Ievadiet vērtību");
+
+    switch(y) {
+        case "5":
+            alert("y ir vienāds ar 5");
+            break;
+
+        case "6":
+            alert("y ir vienāds ar 6")
+            break;
+
+        default:
+            alert("nav vērtības");
+            }
