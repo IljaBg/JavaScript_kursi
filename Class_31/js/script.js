3fe67aa625a4c666c9e857becd564723a911a8f4
IljaBg <ilja.buglovs@gmail.com>
2022-09-12 21:05:06 +0300

    Class_31c

--- Class_31/js/script.js
+++ Class_31/js/script.js
@@ -1,2 +1,17 @@
-const str = document.cookie;
-document.cookie = "username=admin";
+expires = new Date('2022-09-12T20:23:00.000');
+
+document.cookie = `username=admin; path=/; expires=${expires.toGMTString()}`;
+
+// let intervalID = setInterval(check_cookie, 1000);
+
+function check_cookie(){
+  var current = new Date();
+  console.log(current);
+  const str = document.cookie;
+  console.log(str);
+  if(str==''){
+    clearInterval(intervalID);
+  }
+}
+
+
