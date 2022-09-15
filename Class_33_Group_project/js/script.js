0d186cec1c269a0eae62ee54f6e3cc0b77650bda
IljaBg <ilja.buglovs@gmail.com>
2022-09-15 20:59:01 +0300

    Class_33

--- Class_33_Group_project/js/script.js
+++ Class_33_Group_project/js/script.js
@@ -1,3 +1,5 @@
+// https://vanillawebprojects.com/projects/exchange-rate/
+
 const currencyEl_one = document.getElementById('currency-one');
 const amountEl_one = document.getElementById('amount-one');
 const currencyEl_two = document.getElementById('currency-two');
