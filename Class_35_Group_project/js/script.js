0946a25a7edb3e8b3cce3ebf3fc2098d6d267012
IljaBg <ilja.buglovs@gmail.com>
2022-09-21 21:36:55 +0300

    Class_35

---
+++ Class_35_Group_project/js/script.js
@@ -0,0 +1,16 @@
+// // from https://webdesign.tutsplus.com/tutorials/how-to-build-a-simple-carousel-with-vanilla-javascript--cms-41734	
+
+const slidesContainer = document.getElementById("slides-container");
+const slide = document.querySelector(".slide");
+const prevButton = document.getElementById("slide-arrow-prev");
+const nextButton = document.getElementById("slide-arrow-next");
+ 
+nextButton.addEventListener("click", () => {
+  const slideWidth = slide.clientWidth;
+  slidesContainer.scrollLeft += slideWidth;
+});
+ 
+prevButton.addEventListener("click", () => {
+  const slideWidth = slide.clientWidth;
+  slidesContainer.scrollLeft -= slideWidth;
+});
