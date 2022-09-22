c12c69658c2dfe7efbc6d6deac60f7d421d7e205
IljaBg <ilja.buglovs@gmail.com>
2022-09-22 21:17:18 +0300

    Class_36test2

--- Class_36/js/isInteger.js
+++ Class_36/js/isInteger.js
@@ -1,3 +1,13 @@
 //module.exports = (value) => !isNaN(parseInt(value, 10));
 
 module.exports = (value) => Number.isInteger(value);
+
+const time = new Date().getHours();
+let greeting;
+if (time < 10) {
+  greeting = "Labrīt!";
+} else if (time < 20) {
+  greeting = "Labdien!";
+} else {
+  greeting = "Labvakar!";
+}
