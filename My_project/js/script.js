6d2b6e43b248b30dbbba7d185458bbb2955146c9
IljaBg <ilja.buglovs@gmail.com>
2022-09-28 18:38:43 +0300

    Class_38

--- My_project/js/script.js
+++ My_project/js/script.js
@@ -42,7 +42,7 @@ replay.addEventListener("click", function() {
 const restartSong = song =>{
     let currentTime = song.currentTime;
     song.currentTime = 0;
-    console.log("ciao")
+    console.log("replay")
 
 }
 
---
+++ My_project/readme.md
@@ -0,0 +1,4 @@
+Meditācijas web-app
+
+Ievadlapa (index) + scripts (aptauja/īpašību izvēlne)
+-> divi linki 1) rain 2) beach ar scriptu (5, 10, 20 minūtes)
